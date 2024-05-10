4d6e4e0b46e02af1beeb8835e5864b1f95366650
kiibra <kibra99@gmail.com>
2024-05-10 19:40:36 -0400

    complete show and update routes and controller functions

--- controllers/devices.js
+++ controllers/devices.js
@@ -23,7 +23,7 @@ async function create(req, res) {
 
 async function index(req, res) {
   try {
-    const devices = await Devices.find({})
+    const devices = await Device.find({})
       .populate('author')
       .sort({ createdAt: 'desc' })
     res.status(200).json(devices)
@@ -33,7 +33,40 @@ async function index(req, res) {
   }
 }
 
+async function show(req, res) {
+  try {
+    const device = await Device.findById(req.params.deviceId)
+      .populate(['author', 'offers.author'])
+    res.status(200).json(device)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+async function update(req, res) {
+  try {
+    const deviceToCheck = await Device.findById(req.params.deviceId)
+    // this protects devices to make sure the user is the creater
+    if (deviceToCheck.author.equals(req.user.profile)) {
+      console.log('author is a match')
+      const device = await Device.findByIdAndUpdate(
+        req.params.deviceId,
+        req.body,
+        {new: true}
+      ).populate('author')
+      res.status(200).json(device)
+    } else {
+      res.status(401).json({error: 'Not Authorized'})
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
 export{
   create,
   index,
+  show,
+  update,
 }
