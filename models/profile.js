542a4f80b675ce28d58f6253ac780d4cdb4d8902
Seun Dada <seun.dada777@gmail.com>
2024-05-10 21:37:04 -0400

    add reference for profile model to device model

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  devices: { type: Schema.Types.ObjectId, ref: 'Device' },
 },{
   timestamps: true,
 })
